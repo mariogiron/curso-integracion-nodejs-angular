db357808bd5673b931b0f58fe8a84cafef1fab99
Juan Antonio Pérez Jarillo <jantonio.perez@estudioclandestino.es>
2025-04-09 20:04:19 +0200

    proyecto de gestion documental v6

--- app-gestion-doc/backend/app.js
+++ app-gestion-doc/backend/app.js
@@ -1,7 +1,11 @@
+const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+
+//configuracion socketIO
+const socketIO = require('socket.io');
 
 const app = express();
 app.use(cors());
@@ -17,7 +21,28 @@ mongoose.connect(process.env.MONGO_URI)
 // ruta de entrada al endPoint principal.
 app.use('/api/documents', require('./routes/documents.route'))
 
+//necesario para que SocketIo funcion crear un servidor http
+const server = http.createServer(app);
+
 //inicializamos el servidor
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`))
+server.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`))
+
+//socket io: emisiones y recepción de datos.
+// evento que me permite conectar el canal de comunicacion. Cada que un cliente se conecte lo a atraves de este canal para luego poder emitir sus acciones.
+const io = socketIO(server, {
+    cors: { origin: "*" }
+})
+
+io.on('connection', async (socket) => {
+    console.log('nueva conexion')
+
+    //ponerme a escuchar lo que los usuario me manden como chat_message
+    socket.on('chat_message', (data) => {
+        //console.log(data);
+        //emitieramos al resto de usuario conectado los mensajes recibidos.
+        io.emit('chat_message_server', data)
+    })
+
+})
 
--- app-gestion-doc/frontend/src/app/components/chat/chat.component.ts
+++ app-gestion-doc/frontend/src/app/components/chat/chat.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { io } from 'socket.io-client';
+import { environment } from '../../../environments/environment.development';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
+  socket = io(environment.baseUrl);
+  arrMessages: any[] = [];
+  chatForm: FormGroup = new FormGroup({
+    name: new FormControl("", [
+      Validators.required
+    ]),
+    message: new FormControl("", [
+      Validators.required
+    ])
+  }, [])
+
+  ngOnInit() {
+    this.socket.on('chat_message_server', (data: any) => {
+      //se ejecuta cada vez que el servidor emita un mensaje de otro usuario
+      this.arrMessages.push(data);
+    })
+
+  }
+
+  getMessage() {
+    this.socket.emit('chat_message', this.chatForm.value)
+    this.chatForm.get('message')?.reset()
+  }
 
 }
